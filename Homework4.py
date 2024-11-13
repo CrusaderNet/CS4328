8571d37cebf753e3c1f38fd8890cb4ed7941a910
CrusaderNet <110443549+CrusaderNet@users.noreply.github.com>
2024-11-13 15:48:34 -0600

    edited to remove linear SVC

--- Homework4.py
+++ Homework4.py
@@ -53,7 +53,6 @@ models = [
     (LogisticRegression(), "Logistic Regression"),
     (DecisionTreeClassifier(), "Decision Tree"),
     (svm.SVC(kernel='rbf'), "SVC (RBF Kernel)"),
-    #(svm.SVC(kernel='linear'), "SVC (Linear Kernel)"),
     (RandomForestClassifier(), "Random Forest")
 ]
 
